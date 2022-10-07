e88661dfb5001c4338d39cdac5bd7c5458de5716
Muhammad Usman <muhammad.usman@gagamuller.com>
2022-10-08 03:36:37 +0500

    server project path set

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/vetsched'));
+app.use(express.static(__dirname + '/dist/project'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/vetsched/index.html'));});
+'/dist/project/index.html'));});
 app.listen(process.env.PORT || 8080);
